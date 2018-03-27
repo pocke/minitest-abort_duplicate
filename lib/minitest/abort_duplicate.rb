0e6fb1f7947cadc388348f1ecad1aef02a55c72c
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-03-27 11:23:59 +0900

    Implement

--- lib/minitest/abort_duplicate.rb
+++ lib/minitest/abort_duplicate.rb
@@ -2,6 +2,29 @@ require "minitest/abort_duplicate/version"
 
 module Minitest
   module AbortDuplicate
-    # Your code goes here...
+    class << self
+      attr_writer :files
+
+      def files
+        (defined?(@files) ? @files : Dir.glob('**/*')).map {|f| File.absolute_path(f) }
+      end
+    end
+
+    module WarningMixin
+      Warning.extend self
+
+      def warn(message)
+        if match = regexp.match(message)
+          puts "abort: #{match[:method_name]} is redefined from #{match[:location]}. It conceals another test case. Rename this (or another) method name to avoid concealing!"
+          exit 1
+        end
+
+        super
+      end
+
+      def regexp
+        @regexp ||= /\A(?<location>#{Regexp.union(Minitest::AbortDuplicate.files)}:\d+): warning: method redefined; discarding old (?<method_name>test_.*)/
+      end
+    end
   end
 end
